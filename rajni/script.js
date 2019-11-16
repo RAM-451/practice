533e57134a6259c9c8f24649d97b53cdddba2452
akhilesh132 <rajakhil132@gmail.com>
2019-11-16 11:28:13 +0530

    first commit by akhilesh

--- rajni/script.js
+++ rajni/script.js
@@ -1,17 +1,8 @@
 
-$(document).ready(function(){
-      $("button").click(function(){
-            alert($("button").text());
+
+for (let buttonNum = 1; buttonNum <= 10; buttonNum++){
+      document.write(`<button id=b${buttonNum}>${buttonNum}</button>`);
+      $(`#b${buttonNum}`).click(function(){
+            alert($(`#b${buttonNum}`).text());
       })
-
-}
-
-      
-      
-      
-      
-      
-      
-      
-      
-      )
+} 
