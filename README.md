b56cf529ebcd2107cc79ca9192bef33c6d0ed8f4
RAM-451 <ravikantraj598@gmail.com>
2019-11-22 14:29:44 +0530

    changing  js file q
    cd.
    
    ;l jfdse

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # practice
 ## 21.11.2019 just updating this file
+## 22.11.2019 bassically  here I am coverting jQuery to DOM
--- rajni/script.js
+++ rajni/script.js
@@ -1,8 +1,19 @@
 
 
-for (let buttonNum = 1; buttonNum <= 10; buttonNum++){
-      document.write(`<button id=b${buttonNum}>${buttonNum}</button>`);
-      $(`#b${buttonNum}`).click(function(){
-            alert($(`#b${buttonNum}`).text());
+// for (let buttonNum = 1; buttonNum <= 10; buttonNum++){
+//       document.write(`<button id=b${buttonNum}>${buttonNum}</button>`);
+//       $(`#b${buttonNum}`).click(function(){
+//             alert($(`#b${buttonNum}`).text());
+//       })
+// } 
+for(let buttonNum =1;buttonNum<=20;buttonNum++){
+document.write(`<button id=btn${buttonNum} >`+buttonNum+`</button>`);
+
+      var x=document.querySelector(`#btn${buttonNum}`)
+      x.addEventListener('click',function(){
+            alert(buttonNum)
+
+
       })
-} 
+
+}
