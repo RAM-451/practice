f8ca9242338af2cba99839801e38e931c8774ed5
akhilesh132 <rajakhil132@gmail.com>
2019-11-25 23:31:24 +0530

    refactor deciToBin.js

--- rajni/deciToBin.js
+++ rajni/deciToBin.js
@@ -1,18 +1,14 @@
-function binToDeci(){
-      var arr=[]
-      var num=33,x,rem;
-      x=num;
-      while(x!=0){
-            r=x%2;
-            x=parseInt(x/2);
-            arr.unshift(r)
-      }
+function decimalToBinary(num){
+    let arr=[]
+    let x,r;
+    x=num;
+    while(x !== 0){
+        r=x%2;
+        x=Math.floor(x/2);
+        arr.unshift(r)
+     }
     
-      var binary=arr.join("")
-//      var bin=binary.join("and")
-      var r=parseInt(binary)
-      console.log(r);
-    
-
+     return arr;  
 }
-binToDeci()
+
+console.log(decimalToBinary(38));
